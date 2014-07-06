5304dfdc500b700fbad6874e407751c731f41aef
Daryl Moulder <dmoulder@qtome.com>
2014-07-06 16:59:33 +1000

    remove pyc files

--- tests.py
+++ tests.py
@@ -14,14 +14,9 @@ class SamplebossTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(5000000000000000)
-
-    def tearDown(self):
-        #self.browser.quit()
-        print "finished"
+        self.browser.implicitly_wait(5000000)
 
     def test_can_search_the_web_using_boss(self):
-        # Gertrude opens her web browser, and goes to the admin page
         self.browser.get(self.live_server_url + '/search/')
 
         text_input = self.browser.find_element_by_name('q')
@@ -29,7 +24,7 @@ class SamplebossTest(LiveServerTestCase):
         search_btn = self.browser.find_element_by_id('sendbutton')
         search_btn.click()
         try:
-            element = WebDriverWait(self.browser, 5000000000000000).until(
+            element = WebDriverWait(self.browser, 5000000).until(
             EC.presence_of_element_located((By.ID, "resultstable")))
             assert element.tag_name == 'table'
         finally:
